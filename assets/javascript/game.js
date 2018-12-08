f0068509d5de7e53e21b0d2e706cb756b9a812ed
mike929 <mike929@gmail.com>
2018-12-08 13:13:00 -0500

    cleaned up some code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 // Creates an array that lists out all of the options (letters to be guessed).
-var computerChoices = ["r", "p", "s", "t", "l", "j"];
+// var computerChoices = ["r", "p", "s", "t", "l", "j"];
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Creating variables to hold the number of wins, losses, and ties. They start at 0.
 var wins = 0;
@@ -12,7 +13,10 @@ var resetGuesses = function () {
     Guesses = 9;
     allUserGuess = ""; 
     document.querySelector("#Guesses").innerHTML = '';
+    computerGuess=computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log(computerGuess);
 }   
+
 // Create variables that hold references to the places in the HTML where we want to display things.
 var directionsText = document.getElementById("GameDirections");
 var userChoiceText = document.getElementById("GuessesLeft");
@@ -21,23 +25,26 @@ var winsText = document.getElementById("Wins");
 var lossesText = document.getElementById("Losses");
 var GuessesText = document.getElementById("Guesses");
 
+    // Randomly chooses a choice from the options array.
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log("Computer Guess " + computerGuess);
+
 // This function is run whenever the user presses a key.
 document.onkeyup = function (event) {
 
     // Determines which key was pressed.
     var userGuess = event.key;
     // var yourGuess = userGuess;
-console.log(userGuess);
+// console.log(userGuess);
 
-    // Randomly chooses a choice from the options array.
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
 
     // determines the outcome of the game (win/loss/tries), and increments the appropriate number
 
 
     // while (Guesses >= 0) {
 
-        if ((computerGuess === "r") || (computerGuess === "p") || (computerGuess === "s") || (computerGuess === "t") || (computerGuess === "l") || (computerGuess === "j")) {
+        // if ((computerGuess === "r") || (computerGuess === "p") || (computerGuess === "s") || (computerGuess === "t") || (computerGuess === "l") || (computerGuess === "j")) {
 
         // for (var tries = 0; tries < computerGuess; tries++) {
 
@@ -50,7 +57,7 @@ console.log(userGuess);
                 losses++;
                 resetGuesses();
         }
-        console.log(computerGuess);
+        // console.log(computerGuess);
 
         allUserGuess = allUserGuess + " " + userGuess;
 
@@ -61,7 +68,7 @@ console.log(userGuess);
         lossesText.textContent = "Losses: " + losses;
         GuessesText.textContent = "GuessesLeft: " + Guesses--;
         // document.querySelector("#GuessesLeft").innerHTML + userGuess;
-    }
-console.log(Guesses);
+    };
+// console.log(Guesses);
     
-};
+// };
